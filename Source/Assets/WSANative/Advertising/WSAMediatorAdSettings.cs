906849c44c15df310f9306dd6ba7f61f7028eced
Clayton, Michael <Michael.Clayton@sky.uk>
2016-04-07 13:46:44 +0100

    Start adding ad mediator ads

---
+++ Source/Assets/WSANative/Advertising/WSAMediatorAdSettings.cs
@@ -0,0 +1,27 @@
+﻿using CI.WSANative.Advertising;
+
+namespace Assets.WSANative.Advertising
+{
+    public class WSAMediatorAdSettings
+    {
+        public string AppId
+        {
+            get; set;
+        }
+
+        public int Width
+        {
+            get; set;
+        }
+
+        public int Height
+        {
+            get; set;
+        }
+
+        public WSAAdPlacement Placement
+        {
+            get; set;
+        }
+    }
+}
---
+++ Source/Assets/WSANative/Advertising/WSANativeMediatorAd.cs
@@ -0,0 +1,103 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//  
+// @module WSA Native for Unity3D 
+// @author Michael Clayton
+// @support clayton.inds+support@gmail.com 
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace CI.WSANative.Advertising
+{
+    public static class WSANativeMediatorAd
+    {
+        /// <summary>
+        /// Raised when a new ad is received
+        /// </summary>
+        public static Action AdRefreshed
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Raised when the user interacts with an ad
+        /// </summary>
+        public static Action IsEngagedChanged
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Raised when the ad encounters an error in operations
+        /// </summary>
+        public static Action ErrorOccurred
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        public static Action<WSABannerAdSettings> Create;
+
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        public static Action Destroy;
+
+        /// <summary>
+        /// Create a banner ad
+        /// </summary>
+        /// <param name="appId">Your apps id</param>
+        /// <param name="adUnitId">Your ad unit id</param>
+        /// <param name="width">Width of the ad</param>
+        /// <param name="height">Height of the ad</param>
+        /// <param name="placement">Where should the ad be placed</param>
+        public static void CreatAd(string appId, string adUnitId, int width, int height, WSAAdPlacement placement)
+        {
+#if NETFX_CORE
+            if (Create != null)
+            {
+                UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+                {
+                    Create(new WSAMediatorAdSettings() { AppId = appId, AdUnitId = adUnitId, Width = width, Height = height, Placement = placement });
+                }, false);
+        }
+#endif
+        }
+
+        /// <summary>
+        /// Destroy the banner ad
+        /// </summary>
+        public static void DestroyAd()
+        {
+#if NETFX_CORE
+            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+            {
+                if (Destroy != null)
+                {
+                    Destroy();
+                }
+            }, false);
+#endif
+        }
+
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        /// <param name="action"></param>
+        public static void RaiseActionOnAppThread(Action action)
+        {
+#if NETFX_CORE
+            UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+            {
+                if (action != null)
+                {
+                    action();
+                }
+            }, false);
+#endif
+        }
+    }
+}
