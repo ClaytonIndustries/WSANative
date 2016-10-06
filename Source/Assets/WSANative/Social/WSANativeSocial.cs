ebcd255489562d859d162788f60cc15416a91193
Michael Clayton <michael.clayton@sky.uk>
2016-10-06 13:17:35 +0100

    Add ability to launch feedback hub

--- Source/Assets/WSANative/Social/WSANativeSocial.cs
+++ Source/Assets/WSANative/Social/WSANativeSocial.cs
@@ -13,10 +13,23 @@ using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage.Streams;
 #endif
 
+using System;
+using System.Collections.Generic;
+
 namespace CI.WSANative.Social
 {
     public static class WSANativeSocial
     {
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        public static Action<Dictionary<string, string>> _ShowFeedbackHub;
+
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        public static Func<bool> _IsFeedbackHubSupported;
+
         /// <summary>
         /// Shows the store description page for you app
         /// </summary>
@@ -103,6 +116,36 @@ namespace CI.WSANative.Social
 
                 DataTransferManager.ShowShareUI();
             }, false);
+#endif
+        }
+
+        /// <summary>
+        /// Returns whether the users device supports the feedback hub (i.e they are runnings Windows 10 build 10.0.14271 or later)
+        /// </summary>
+        /// <returns>Is the feedback hub supported</returns>
+        public static bool IsFeedbackHubSupported()
+        {
+#if UNITY_WSA_10_0
+            return _IsFeedbackHubSupported();
+#else
+            return false;
+#endif
+        }
+
+        /// <summary>
+        /// Shows the feedback hub if it is available
+        /// </summary>
+        /// <param name="feedbackProperties">Metadata that you want to associate with the feedback (does not need to be specified)</param>
+        public static void ShowFeedbackHub(Dictionary<string, string> feedbackProperties = null)
+        {
+#if UNITY_WSA_10_0
+            if (_ShowFeedbackHub != null)
+            {
+                UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+                {
+                    _ShowFeedbackHub(feedbackProperties);
+                }, false);
+            }
 #endif
         }
     }
