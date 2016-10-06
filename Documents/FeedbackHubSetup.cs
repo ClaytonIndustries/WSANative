44787080c1189a75300d379c77711b40fc22b92d
Michael Clayton <michael.clayton@sky.uk>
2016-10-06 13:36:05 +0100

    Move feedback hub to new class

---
+++ Documents/FeedbackHubSetup.cs
@@ -0,0 +1,27 @@
+private void InitialiseFeedbackHub()
+{
+	WSANativeSocial._IsFeedbackHubSupported += () => 
+	{
+		return StoreServicesFeedbackLauncher.IsSupported();
+	};
+	WSANativeSocial._ShowFeedbackHub += async (feedbackProperties) =>
+	{
+		if (StoreServicesFeedbackLauncher.IsSupported())
+		{
+			if (feedbackProperties != null)
+			{
+				await StoreServicesFeedbackLauncher.GetDefault().LaunchAsync(feedbackProperties);
+			}
+			else
+			{
+				await StoreServicesFeedbackLauncher.GetDefault().LaunchAsync();
+			}
+		}
+	};
+}
+
+Add a reference to the Store Engagement SDK
+Add using CI.WSANative.Engagement;
+Add using Microsoft.Services.Store.Engagement;
+Add the code above
+Call the above function from the bottom of the MainPage.xaml.cs constructor
