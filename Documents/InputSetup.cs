34c8d4e2e823d2cfd4c23eac41792d3f7ed1da5e
Michael Clayton <michael.clayton@sky.uk>
2016-10-17 16:37:02 +0100

    Update input setup

--- Documents/InputSetup.cs
+++ Documents/InputSetup.cs
@@ -2,32 +2,38 @@ PointerPressed="DXSwapChainPanel_PointerPressed" PointerReleased="DXSwapChainPan
 
 private void DXSwapChainPanel_PointerPressed(object sender, PointerRoutedEventArgs e)
 {
-	PointerPointProperties pointerProperties = e.GetCurrentPoint(DXSwapChainPanel).Properties;
-
-	WSANativeInput.PointerPressed(new WSAPointerProperties()
+	if (WSANativeInput.PointerPressed != null)
 	{
-		InputType = e.Pointer.PointerDeviceType == PointerDeviceType.Touch ? WSAInputType.Touch : 
-			e.Pointer.PointerDeviceType == PointerDeviceType.Pen ? WSAInputType.Pen : WSAInputType.Mouse,
-		IsLeftButtonPressed = pointerProperties.IsLeftButtonPressed,
-		IsRightButtonPressed = pointerProperties.IsRightButtonPressed,
-		IsEraser = pointerProperties.IsEraser,
-		IsInverted = pointerProperties.IsInverted
-	});
+		PointerPointProperties pointerProperties = e.GetCurrentPoint(DXSwapChainPanel).Properties;
+
+		WSANativeInput.PointerPressed(new WSAPointerProperties()
+		{
+			InputType = e.Pointer.PointerDeviceType == PointerDeviceType.Touch ? WSAInputType.Touch :
+				e.Pointer.PointerDeviceType == PointerDeviceType.Pen ? WSAInputType.Pen : WSAInputType.Mouse,
+			IsLeftButtonPressed = pointerProperties.IsLeftButtonPressed,
+			IsRightButtonPressed = pointerProperties.IsRightButtonPressed,
+			IsEraser = pointerProperties.IsEraser,
+			IsInverted = pointerProperties.IsInverted
+		});
+	}
 }
 
 private void DXSwapChainPanel_PointerReleased(object sender, PointerRoutedEventArgs e)
 {
-	PointerPointProperties pointerProperties = e.GetCurrentPoint(DXSwapChainPanel).Properties;
-
-	WSANativeInput.PointerPressed(new WSAPointerProperties()
+	if (WSANativeInput.PointReleased != null)
 	{
-		InputType = e.Pointer.PointerDeviceType == PointerDeviceType.Touch ? WSAInputType.Touch :
-			e.Pointer.PointerDeviceType == PointerDeviceType.Pen ? WSAInputType.Pen : WSAInputType.Mouse,
-		IsLeftButtonPressed = pointerProperties.IsLeftButtonPressed,
-		IsRightButtonPressed = pointerProperties.IsRightButtonPressed,
-		IsEraser = pointerProperties.IsEraser,
-		IsInverted = pointerProperties.IsInverted
-	});
+		PointerPointProperties pointerProperties = e.GetCurrentPoint(DXSwapChainPanel).Properties;
+
+		WSANativeInput.PointReleased(new WSAPointerProperties()
+		{
+			InputType = e.Pointer.PointerDeviceType == PointerDeviceType.Touch ? WSAInputType.Touch :
+				e.Pointer.PointerDeviceType == PointerDeviceType.Pen ? WSAInputType.Pen : WSAInputType.Mouse,
+			IsLeftButtonPressed = pointerProperties.IsLeftButtonPressed,
+			IsRightButtonPressed = pointerProperties.IsRightButtonPressed,
+			IsEraser = pointerProperties.IsEraser,
+			IsInverted = pointerProperties.IsInverted
+		});
+	}
 }
 
 using CI.WSANative.Input;
