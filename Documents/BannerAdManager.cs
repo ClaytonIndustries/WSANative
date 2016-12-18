2dc9b733c87a544b0fdfb743be4b258b710dec1c
Michael Clayton <mike-16@hotmail.co.uk>
2016-12-18 15:37:20 +0000

    Add banner and interstitial ad managers for .NET

---
+++ Documents/BannerAdManager.cs
@@ -0,0 +1,125 @@
+﻿//Uncomment the ones you want to enable
+//#define BAM_AD_DUPLEX_ENABLED
+//#define BAM_MICROSOFT_ENABLED
+
+#if BAM_AD_DUPLEX_ENABLED
+//Uncomment the correct using
+//using AdDuplexAd = AdDuplex;                                    (Windows 10)
+//using AdDuplexAd = AdDuplex.Universal.Controls.Win.XAML;        (Windows 8.1 Desktop)
+//using AdDuplexAd = AdDuplex.Universal.Controls.WinPhone.XAML;   (Windows 8.1 Mobile)
+#endif
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CI.WSANative.Advertising
+{
+    public static class BannerAdManager
+    {
+        public static void Initialise(Grid grid)
+        {
+#if BAM_AD_DUPLEX_ENABLED
+            ConfigureAdDuplexBannerAd(grid);
+#endif
+#if BAM_MICROSOFT_ENABLED
+            ConfigureMicrosoftBannerAd(grid);
+#endif
+        }
+
+#if BAM_AD_DUPLEX_ENABLED
+        private static void ConfigureAdDuplexBannerAd(Grid grid)
+        {
+            AdDuplexAd.AdControl adControl = null;
+            WSANativeBannerAd.Create += (bannerAdSettings) =>
+            {
+                if (bannerAdSettings.AdType == WSABannerAdType.AdDuplex && adControl == null)
+                {
+                    adControl = new AdDuplexAd.AdControl();
+                    adControl.AppKey = bannerAdSettings.AppId;
+                    adControl.AdUnitId = bannerAdSettings.AdUnitId;
+                    adControl.Width = bannerAdSettings.Width;
+                    adControl.Height = bannerAdSettings.Height;
+                    adControl.RefreshInterval = 30;
+                    adControl.VerticalAlignment = bannerAdSettings.VerticalPlacement == WSAAdVerticalPlacement.Top ? VerticalAlignment.Top
+                        : bannerAdSettings.VerticalPlacement == WSAAdVerticalPlacement.Bottom ? VerticalAlignment.Bottom : VerticalAlignment.Center;
+                    adControl.HorizontalAlignment = bannerAdSettings.HorizontalPlacement == WSAAdHorizontalPlacement.Left ? HorizontalAlignment.Left
+                        : bannerAdSettings.HorizontalPlacement == WSAAdHorizontalPlacement.Right ? HorizontalAlignment.Right : HorizontalAlignment.Center;
+                    adControl.AdLoaded += (s, e) => { WSANativeBannerAd.RaiseActionOnAppThread(WSANativeBannerAd.AdRefreshed, WSABannerAdType.AdDuplex); };
+                    adControl.AdCovered += (s, e) => { WSANativeBannerAd.RaiseActionOnAppThread(WSANativeBannerAd.ErrorOccurred, WSABannerAdType.AdDuplex); };
+                    adControl.AdLoadingError += (s, e) => { WSANativeBannerAd.RaiseActionOnAppThread(WSANativeBannerAd.ErrorOccurred, WSABannerAdType.AdDuplex); };
+                    adControl.NoAd += (s, e) => { WSANativeBannerAd.RaiseActionOnAppThread(WSANativeBannerAd.ErrorOccurred, WSABannerAdType.AdDuplex); };
+                    grid.Children.Add(adControl);
+                }
+            };
+            WSANativeBannerAd.SetVisiblity += (adType, visible) =>
+            {
+                if (adType == WSABannerAdType.AdDuplex && adControl != null)
+                {
+                    adControl.Visibility = visible ? Visibility.Visible : Visibility.Collapsed;
+                }
+            };
+            WSANativeBannerAd.Destroy += (adType) =>
+            {
+                if (adType == WSABannerAdType.AdDuplex && adControl != null)
+                {
+                    foreach (UIElement child in grid.Children)
+                    {
+                        if (child == adControl)
+                        {
+                            grid.Children.Remove(child);
+                            adControl = null;
+                            break;
+                        }
+                    }
+                }
+            };
+        }
+#endif
+#if BAM_MICROSOFT_ENABLED
+        private static void ConfigureMicrosoftBannerAd(Grid grid)
+        {
+            Microsoft.Advertising.WinRT.UI.AdControl adControl = null;
+            WSANativeBannerAd.Create += (bannerAdSettings) =>
+            {
+                if (bannerAdSettings.AdType == WSABannerAdType.Microsoft && adControl == null)
+                {
+                    adControl = new Microsoft.Advertising.WinRT.UI.AdControl();
+                    adControl.ApplicationId = bannerAdSettings.AppId;
+                    adControl.AdUnitId = bannerAdSettings.AdUnitId;
+                    adControl.Width = bannerAdSettings.Width;
+                    adControl.Height = bannerAdSettings.Height;
+                    adControl.IsAutoRefreshEnabled = true;
+                    adControl.VerticalAlignment = bannerAdSettings.VerticalPlacement == WSAAdVerticalPlacement.Top ? VerticalAlignment.Top
+                        : bannerAdSettings.VerticalPlacement == WSAAdVerticalPlacement.Bottom ? VerticalAlignment.Bottom : VerticalAlignment.Center;
+                    adControl.HorizontalAlignment = bannerAdSettings.HorizontalPlacement == WSAAdHorizontalPlacement.Left ? HorizontalAlignment.Left
+                        : bannerAdSettings.HorizontalPlacement == WSAAdHorizontalPlacement.Right ? HorizontalAlignment.Right : HorizontalAlignment.Center;
+                    adControl.AdRefreshed += (s, e) => { WSANativeBannerAd.RaiseActionOnAppThread(WSANativeBannerAd.AdRefreshed, WSABannerAdType.Microsoft); };
+                    adControl.ErrorOccurred += (s, e) => { WSANativeBannerAd.RaiseActionOnAppThread(WSANativeBannerAd.ErrorOccurred, WSABannerAdType.Microsoft); };
+                    grid.Children.Add(adControl);
+                }
+            };
+            WSANativeBannerAd.SetVisiblity += (adType, visible) =>
+            {
+                if (adType == WSABannerAdType.Microsoft && adControl != null)
+                {
+                    adControl.Visibility = visible ? Visibility.Visible : Visibility.Collapsed;
+                }
+            };
+            WSANativeBannerAd.Destroy += (adType) =>
+            {
+                if (adType == WSABannerAdType.Microsoft && adControl != null)
+                {
+                    foreach (UIElement child in grid.Children)
+                    {
+                        if (child == adControl)
+                        {
+                            grid.Children.Remove(child);
+                            adControl = null;
+                            break;
+                        }
+                    }
+                }
+            };
+        }
+#endif
+    }
+}
---
+++ Documents/InterstitialAdManager.cs
@@ -0,0 +1,138 @@
+﻿//Uncomment the ones you want to enable
+//#define IAM_AD_DUPLEX_ENABLED
+//#define IAM_MICROSOFT_ENABLED
+//#define IAM_VUNGLE_ENABLED
+
+using System;
+using UnityPlayer;
+
+namespace CI.WSANative.Advertising
+{
+    public static class InterstitialAdManager
+    {
+        public static void Initialise()
+        {
+#if IAM_AD_DUPLEX_ENABLED
+            ConfigureAdDuplexInterstitalAd();
+#endif
+#if IAM_MICROSOFT_ENABLED
+            ConfigureMicrosoftInterstitalAd();
+#endif
+#if IAM_VUNGLE_ENABLED
+            ConfigureVungleInterstitalAd();
+#endif
+        }
+
+#if IAM_AD_DUPLEX_ENABLED
+        private static void ConfigureAdDuplexInterstitalAd()
+        {
+            AdDuplex.InterstitialAd interstitialAd = null;
+            WSANativeInterstitialAd._Request += (adType, appId, adUnitId) =>
+            {
+                if (adType == WSAInterstitialAdType.AdDuplex)
+                {
+                    AppCallbacks.Instance.InvokeOnUIThread(async () =>
+                    {
+                        if (interstitialAd == null)
+                        {
+                            AdDuplex.AdDuplexClient.Initialize(appId);
+                            interstitialAd = new AdDuplex.InterstitialAd(adUnitId);
+                            interstitialAd.AdLoaded += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.AdReady, WSAInterstitialAdType.AdDuplex); };
+                            interstitialAd.AdClosed += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.Completed, WSAInterstitialAdType.AdDuplex); };
+                            interstitialAd.AdLoadingError += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.ErrorOccurred, WSAInterstitialAdType.AdDuplex); };
+                            interstitialAd.NoAd += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.ErrorOccurred, WSAInterstitialAdType.AdDuplex); };
+                        }
+
+                        await interstitialAd.LoadAdAsync();
+                    }, false);
+                }
+            };
+            WSANativeInterstitialAd._Show += (adType) =>
+            {
+                if (adType == WSAInterstitialAdType.AdDuplex)
+                {
+                    AppCallbacks.Instance.InvokeOnUIThread(async () =>
+                    {
+                        await interstitialAd.ShowAdAsync();
+                    }, false);
+                }
+            };
+        }
+#endif
+#if IAM_MICROSOFT_ENABLED
+        private static void ConfigureMicrosoftInterstitalAd()
+        {
+            Microsoft.Advertising.WinRT.UI.InterstitialAd interstitialAd = new Microsoft.Advertising.WinRT.UI.InterstitialAd();
+	        interstitialAd.AdReady += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.AdReady, WSAInterstitialAdType.Microsoft); };
+	        interstitialAd.ErrorOccurred += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.ErrorOccurred, WSAInterstitialAdType.Microsoft); };
+	        interstitialAd.Completed += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.Completed, WSAInterstitialAdType.Microsoft); };
+	        interstitialAd.Cancelled += (s, e) => { WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.Cancelled, WSAInterstitialAdType.Microsoft); };
+	        WSANativeInterstitialAd._Request += (adType, appId, adUnitId) =>
+	        {
+		        if (adType == WSAInterstitialAdType.Microsoft)
+		        {
+			        interstitialAd.RequestAd(Microsoft.Advertising.WinRT.UI.AdType.Video, appId, adUnitId);
+		        }
+	        };
+	        WSANativeInterstitialAd._Show += (adType) =>
+	        {
+		        if (adType == WSAInterstitialAdType.Microsoft && interstitialAd.State == Microsoft.Advertising.WinRT.UI.InterstitialAdState.Ready)
+		        {
+			        AppCallbacks.Instance.InvokeOnUIThread(() =>
+			        {
+				        interstitialAd.Show();
+			        }, false);
+		        }
+	        };
+        }
+#endif
+#if IAM_VUNGLE_ENABLED
+        private static void ConfigureVungleInterstitalAd()
+        {
+            VungleSDK.VungleAd interstitialAd = null;
+	        WSANativeInterstitialAd._Request += (adType, appId, adUnitId) =>
+	        {
+		        if(adType == WSAInterstitialAdType.Vungle && interstitialAd == null)
+		        {
+			        interstitialAd = VungleSDK.AdFactory.GetInstance(appId);
+			        interstitialAd.OnAdPlayableChanged += (s, e) =>
+			        {
+				        if (e.AdPlayable)
+				        {
+					        WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.AdReady, WSAInterstitialAdType.Vungle);
+				        }
+			        };
+			        interstitialAd.OnVideoView += (s, e) =>
+			        {
+				        if (e.IsCompletedView)
+				        {
+					        WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.Completed, WSAInterstitialAdType.Vungle);
+				        }
+				        else
+				        {
+					        WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.Cancelled, WSAInterstitialAdType.Vungle);
+				        }
+			        };
+			        interstitialAd.Diagnostic += (s, e) =>
+			        {
+				        if(e.Level == VungleSDK.DiagnosticLogLevel.Error || e.Level == VungleSDK.DiagnosticLogLevel.Fatal)
+				        {
+					        WSANativeInterstitialAd.RaiseActionOnAppThread(WSANativeInterstitialAd.ErrorOccurred, WSAInterstitialAdType.Vungle);
+				        }
+			        };
+		        }
+	        };
+	        WSANativeInterstitialAd._Show += (adType) =>
+	        {
+		        if (adType == WSAInterstitialAdType.Vungle && interstitialAd != null && interstitialAd.AdPlayable)
+		        {
+			        AppCallbacks.Instance.InvokeOnUIThread(async () =>
+			        {
+				        await interstitialAd.PlayAdAsync(new VungleSDK.AdConfig());
+			        }, false);
+		        }
+	        };
+        }
+#endif
+    }
+}
