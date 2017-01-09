77dd6dabca2e87da4ed710d5034fcfca1fedb395
Michael Clayton <mike-16@hotmail.co.uk>
2017-01-09 21:41:06 +0000

    Hide unused variable warnings in WSANativeBannerAd

--- Source/Assets/WSANative/Advertising/WSANativeBannerAd.cs
+++ Source/Assets/WSANative/Advertising/WSANativeBannerAd.cs
@@ -8,11 +8,178 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 //  
+// IL2CPP Implementation
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#if ENABLE_IL2CPP
+using System;
+using System.Runtime.InteropServices;
+using AOT;
+
+namespace CI.WSANative.Advertising
+{
+    public static class WSANativeBannerAd
+    {
+        private delegate void AdRefreshedCallbackDelegate([MarshalAs(UnmanagedType.LPWStr)]string adType);
+        private delegate void ErrorOccurredCallbackDelegate([MarshalAs(UnmanagedType.LPWStr)]string adType);
+
+        [DllImport("__Internal")]
+        private static extern void _BannerAdInitialise(AdRefreshedCallbackDelegate adRefreshedCallback, ErrorOccurredCallbackDelegate errorOccurredCallback);
+
+        [DllImport("__Internal")]
+        private static extern void _BannerAdCreate([MarshalAs(UnmanagedType.LPWStr)]string adType, [MarshalAs(UnmanagedType.LPWStr)]string appId, 
+            [MarshalAs(UnmanagedType.LPWStr)]string adUnitId, int width, int height, 
+            [MarshalAs(UnmanagedType.LPWStr)]string verticalPlacement, [MarshalAs(UnmanagedType.LPWStr)]string horizontalPlacement);
+
+        [DllImport("__Internal")]
+        private static extern void _BannerAdSetVisibility([MarshalAs(UnmanagedType.LPWStr)]string adType, bool visible);
+
+        [DllImport("__Internal")]
+        private static extern void _BannerAdDestroy([MarshalAs(UnmanagedType.LPWStr)]string adType);
+
+        /// <summary>
+        /// Raised when a new ad is received
+        /// </summary>
+        public static Action<WSABannerAdType> AdRefreshed
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Raised when the ad encounters an error in operations
+        /// </summary>
+        public static Action<WSABannerAdType> ErrorOccurred
+        {
+            get; set;
+        }
+
+#pragma warning disable 0414
+        private static string _msAppId;
+        private static string _msAdUnitId;
+
+        private static string _adDuplexAppId;
+        private static string _adDuplexAdUnitId;
+#pragma warning restore 0414
+
+        /// <summary>
+        /// Initialise the banner ad for the specified provider
+        /// </summary>
+        /// <param name="adType">The ad network to initialise</param>
+        /// <param name="appId">Your apps id</param>
+        /// <param name="adUnitId">Your apps ad unit id</param>
+        public static void Initialise(WSABannerAdType adType, string appId, string adUnitId)
+        {
+            switch (adType)
+            {
+                case WSABannerAdType.AdDuplex:
+                    _adDuplexAppId = appId;
+                    _adDuplexAdUnitId = adUnitId;
+                    break;
+                case WSABannerAdType.Microsoft:
+                    _msAppId = appId;
+                    _msAdUnitId = adUnitId;
+                    break;
+            }
+
+            _BannerAdInitialise(AdRefreshedCallback, ErrorOccurredCallback);
+        }
+
+        /// <summary>
+        /// Create a banner ad
+        /// </summary>
+        /// <param name="adType">The ad type</param>
+        /// <param name="width">Width of the ad</param>
+        /// <param name="height">Height of the ad</param>
+        /// <param name="verticalPlacement">Where should the ad be placed vertically</param>
+        /// <param name="horizontalPlacement">Where should the ad be placed horizontally</param>
+        public static void CreatAd(WSABannerAdType adType, int width, int height, WSAAdVerticalPlacement verticalPlacement, WSAAdHorizontalPlacement horizontalPlacement)
+        {
+#if NETFX_CORE
+            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+            {
+                if (adType == WSABannerAdType.AdDuplex)
+                {
+                    _BannerAdCreate(adType.ToString(), _adDuplexAppId, _adDuplexAdUnitId, width, height, verticalPlacement.ToString(), horizontalPlacement.ToString());
+                }
+                else if(adType == WSABannerAdType.Microsoft)
+                {
+                    _BannerAdCreate(adType.ToString(), _msAppId, _msAdUnitId, width, height, verticalPlacement.ToString(), horizontalPlacement.ToString());
+                }
+            }, false);
+#endif
+        }
+
+        /// <summary>
+        /// Show or hide the banner ad
+        /// </summary>
+        /// <param name="adType">The ad type</param>
+        /// <param name="visible">Should the ad be visible</param>
+        public static void SetAdVisibility(WSABannerAdType adType, bool visible)
+        {
+#if NETFX_CORE
+            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+            {
+                _BannerAdSetVisibility(adType.ToString(), visible);
+            }, false);
+#endif
+        }
+
+        /// <summary>
+        /// Destroy the banner ad
+        /// </summary>
+        /// <param name="adType">The ad type</param>
+        public static void DestroyAd(WSABannerAdType adType)
+        {
+#if NETFX_CORE
+            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+            {
+                _BannerAdDestroy(adType.ToString());
+            }, false);
+#endif
+        }
+
+
+        [MonoPInvokeCallback(typeof(AdRefreshedCallbackDelegate))]
+        private static void AdRefreshedCallback(string adType)
+        {
+            if (AdRefreshed != null)
+            {
+                AdRefreshed(GetAdType(adType));
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(ErrorOccurredCallbackDelegate))]
+        private static void ErrorOccurredCallback(string adType)
+        {
+            if(ErrorOccurred != null)
+            {
+                ErrorOccurred(GetAdType(adType));
+            }
+        }
+
+        private static WSABannerAdType GetAdType(string adType)
+        {
+            if (adType == WSABannerAdType.AdDuplex.ToString())
+            {
+                return WSABannerAdType.AdDuplex;
+            }
+            else
+            {
+                return WSABannerAdType.Microsoft;
+            }
+        }
+    }
+}
+
+#else
+
+////////////////////////////////////////////////////////////////////////////////
+//  
 // .NET Implementation
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-#if ENABLE_DOTNET
 using System;
 
 namespace CI.WSANative.Advertising
@@ -50,11 +217,13 @@ namespace CI.WSANative.Advertising
         /// </summary>
         public static Action<WSABannerAdType> Destroy;
 
+#pragma warning disable 0414
         private static string _adDuplexAppId;
         private static string _adDuplexAdUnitId;
 
         private static string _msAppId;
         private static string _msAdUnitId;
+#pragma warning restore 0414
 
         /// <summary>
         /// Initialise the banner ad for the specified provider
@@ -174,171 +343,6 @@ namespace CI.WSANative.Advertising
                 }, false);
             }
 #endif
-        }
-    }
-}
-#endif
-
-////////////////////////////////////////////////////////////////////////////////
-//  
-// IL2CPP Implementation
-//
-////////////////////////////////////////////////////////////////////////////////
-
-#if ENABLE_IL2CPP
-using System;
-using System.Runtime.InteropServices;
-using AOT;
-
-namespace CI.WSANative.Advertising
-{
-    public static class WSANativeBannerAd
-    {
-        private delegate void AdRefreshedCallbackDelegate([MarshalAs(UnmanagedType.LPWStr)]string adType);
-        private delegate void ErrorOccurredCallbackDelegate([MarshalAs(UnmanagedType.LPWStr)]string adType);
-
-        [DllImport("__Internal")]
-        private static extern void _BannerAdInitialise(AdRefreshedCallbackDelegate adRefreshedCallback, ErrorOccurredCallbackDelegate errorOccurredCallback);
-
-        [DllImport("__Internal")]
-        private static extern void _BannerAdCreate([MarshalAs(UnmanagedType.LPWStr)]string adType, [MarshalAs(UnmanagedType.LPWStr)]string appId, 
-            [MarshalAs(UnmanagedType.LPWStr)]string adUnitId, int width, int height, 
-            [MarshalAs(UnmanagedType.LPWStr)]string verticalPlacement, [MarshalAs(UnmanagedType.LPWStr)]string horizontalPlacement);
-
-        [DllImport("__Internal")]
-        private static extern void _BannerAdSetVisibility([MarshalAs(UnmanagedType.LPWStr)]string adType, bool visible);
-
-        [DllImport("__Internal")]
-        private static extern void _BannerAdDestroy([MarshalAs(UnmanagedType.LPWStr)]string adType);
-
-        /// <summary>
-        /// Raised when a new ad is received
-        /// </summary>
-        public static Action<WSABannerAdType> AdRefreshed
-        {
-            get; set;
-        }
-
-        /// <summary>
-        /// Raised when the ad encounters an error in operations
-        /// </summary>
-        public static Action<WSABannerAdType> ErrorOccurred
-        {
-            get; set;
-        }
-
-        private static string _msAppId;
-        private static string _msAdUnitId;
-
-        private static string _adDuplexAppId;
-        private static string _adDuplexAdUnitId;
-
-        /// <summary>
-        /// Initialise the banner ad for the specified provider
-        /// </summary>
-        /// <param name="adType">The ad network to initialise</param>
-        /// <param name="appId">Your apps id</param>
-        /// <param name="adUnitId">Your apps ad unit id</param>
-        public static void Initialise(WSABannerAdType adType, string appId, string adUnitId)
-        {
-            switch (adType)
-            {
-                case WSABannerAdType.AdDuplex:
-                    _adDuplexAppId = appId;
-                    _adDuplexAdUnitId = adUnitId;
-                    break;
-                case WSABannerAdType.Microsoft:
-                    _msAppId = appId;
-                    _msAdUnitId = adUnitId;
-                    break;
-            }
-
-            _BannerAdInitialise(AdRefreshedCallback, ErrorOccurredCallback);
-        }
-
-        /// <summary>
-        /// Create a banner ad
-        /// </summary>
-        /// <param name="adType">The ad type</param>
-        /// <param name="width">Width of the ad</param>
-        /// <param name="height">Height of the ad</param>
-        /// <param name="verticalPlacement">Where should the ad be placed vertically</param>
-        /// <param name="horizontalPlacement">Where should the ad be placed horizontally</param>
-        public static void CreatAd(WSABannerAdType adType, int width, int height, WSAAdVerticalPlacement verticalPlacement, WSAAdHorizontalPlacement horizontalPlacement)
-        {
-#if NETFX_CORE
-            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
-            {
-                if (adType == WSABannerAdType.AdDuplex)
-                {
-                    _BannerAdCreate(adType.ToString(), _adDuplexAppId, _adDuplexAdUnitId, width, height, verticalPlacement.ToString(), horizontalPlacement.ToString());
-                }
-                else if(adType == WSABannerAdType.Microsoft)
-                {
-                    _BannerAdCreate(adType.ToString(), _msAppId, _msAdUnitId, width, height, verticalPlacement.ToString(), horizontalPlacement.ToString());
-                }
-            }, false);
-#endif
-        }
-
-        /// <summary>
-        /// Show or hide the banner ad
-        /// </summary>
-        /// <param name="adType">The ad type</param>
-        /// <param name="visible">Should the ad be visible</param>
-        public static void SetAdVisibility(WSABannerAdType adType, bool visible)
-        {
-#if NETFX_CORE
-            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
-            {
-                _BannerAdSetVisibility(adType.ToString(), visible);
-            }, false);
-#endif
-        }
-
-        /// <summary>
-        /// Destroy the banner ad
-        /// </summary>
-        /// <param name="adType">The ad type</param>
-        public static void DestroyAd(WSABannerAdType adType)
-        {
-#if NETFX_CORE
-            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
-            {
-                _BannerAdDestroy(adType.ToString());
-            }, false);
-#endif
-        }
-
-
-        [MonoPInvokeCallback(typeof(AdRefreshedCallbackDelegate))]
-        private static void AdRefreshedCallback(string adType)
-        {
-            if (AdRefreshed != null)
-            {
-                AdRefreshed(GetAdType(adType));
-            }
-        }
-
-        [MonoPInvokeCallback(typeof(ErrorOccurredCallbackDelegate))]
-        private static void ErrorOccurredCallback(string adType)
-        {
-            if(ErrorOccurred != null)
-            {
-                ErrorOccurred(GetAdType(adType));
-            }
-        }
-
-        private static WSABannerAdType GetAdType(string adType)
-        {
-            if (adType == WSABannerAdType.AdDuplex.ToString())
-            {
-                return WSABannerAdType.AdDuplex;
-            }
-            else
-            {
-                return WSABannerAdType.Microsoft;
-            }
         }
     }
 }
--- Source/Assets/WSANative/Advertising/WSANativeInterstitialAd.cs
+++ Source/Assets/WSANative/Advertising/WSANativeInterstitialAd.cs
@@ -8,152 +8,6 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 //  
-// .NET Implementation
-//
-////////////////////////////////////////////////////////////////////////////////
-
-#if ENABLE_DOTNET
-using System;
-
-namespace CI.WSANative.Advertising
-{
-    public static class WSANativeInterstitialAd
-    {
-        /// <summary>
-        /// Raised when the interstitial ad is ready to be shown
-        /// </summary>
-        public static Action<WSAInterstitialAdType> AdReady
-        {
-            get; set;
-        }
-
-        /// <summary>
-        /// Raised when the user cancels the interstitial ad before it is considered complete
-        /// </summary>
-        public static Action<WSAInterstitialAdType> Cancelled
-        {
-            get; set;
-        }
-
-        /// <summary>
-        /// Raised when the interstitial ad has been closed and the experience is considered complete
-        /// </summary>
-        public static Action<WSAInterstitialAdType> Completed
-        {
-            get; set;
-        }
-
-        /// <summary>
-        /// Raised when the interstitial ad encounters an operational error
-        /// </summary>
-        public static Action<WSAInterstitialAdType> ErrorOccurred
-        {
-            get; set;
-        }
-
-        /// <summary>
-        /// For internal use only
-        /// </summary>
-        public static Action<WSAInterstitialAdType, string, string> _Request;
-
-        /// <summary>
-        /// For internal use only
-        /// </summary>
-        public static Action<WSAInterstitialAdType> _Show;
-
-        private static string _adDuplexAppId;
-        private static string _adDuplexAdUnitId;
-
-        private static string _msAppId;
-        private static string _msAdUnitId;
-
-        private static string _vungleAppId;
-
-        /// <summary>
-        /// Initialise the interstitial ad for the specified provider
-        /// </summary>
-		/// <param name="adType">The type of ad to request</param>
-        /// <param name="appId">Your apps id</param>
-        /// <param name="adUnitId">Your apps ad unit id (null or empty for Vungle)</param>
-        public static void Initialise(WSAInterstitialAdType adType, string appId, string adUnitId)
-        {
-            switch (adType)
-            {
-                case WSAInterstitialAdType.AdDuplex:
-                    _adDuplexAppId = appId;
-                    _adDuplexAdUnitId = adUnitId;
-                    break;
-                case WSAInterstitialAdType.Microsoft:
-                    _msAppId = appId;
-                    _msAdUnitId = adUnitId;
-                    break;
-                case WSAInterstitialAdType.Vungle:
-                    _vungleAppId = appId;
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Requests an interstitial ad from the server.
-        /// The AdReady event will fire when the request completes successfully.
-        /// Only needs to be called once for Vungle ads (ads will be automatically fetched from then on)
-        /// </summary>
-        /// <param name="adType">The type of ad to request</param>
-        public static void RequestAd(WSAInterstitialAdType adType)
-        {
-            if(_Request != null)
-            {
-                switch (adType)
-                {
-                    case WSAInterstitialAdType.AdDuplex:
-                        _Request(adType, _adDuplexAppId, _adDuplexAdUnitId);
-                        break;
-                    case WSAInterstitialAdType.Microsoft:
-                        _Request(adType, _msAppId, _msAdUnitId);
-                        break;
-                    case WSAInterstitialAdType.Vungle:
-                        _Request(adType, _vungleAppId, string.Empty);
-                        break;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Shows the interstitial ad if it is ready.
-        /// It is recommended that you have at least a 60 second gap between ads
-        /// </summary>
-        /// <param name="adType">The type of ad to show</param>
-        public static void ShowAd(WSAInterstitialAdType adType)
-        {
-            if(_Show != null)
-            {
-                _Show(adType);
-            }
-        }
-
-        /// <summary>
-        /// For internal use only
-        /// </summary>
-        /// <param name="action"></param>
-        /// <param name="adType"></param>
-        public static void RaiseActionOnAppThread(Action<WSAInterstitialAdType> action, WSAInterstitialAdType adType)
-        {
-#if NETFX_CORE
-            if (action != null)
-            {
-                UnityEngine.WSA.Application.InvokeOnAppThread(() =>
-                {
-                    action(adType);
-                }, false);
-            }
-#endif
-        }
-    }
-}
-#endif
-
-////////////////////////////////////////////////////////////////////////////////
-//  
 // IL2CPP Implementation
 //
 ////////////////////////////////////////////////////////////////////////////////
@@ -331,6 +185,152 @@ namespace CI.WSANative.Advertising
             {
                 return WSAInterstitialAdType.Vungle;
             }
+        }
+    }
+}
+
+#else
+
+////////////////////////////////////////////////////////////////////////////////
+//  
+// .NET Implementation
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace CI.WSANative.Advertising
+{
+    public static class WSANativeInterstitialAd
+    {
+        /// <summary>
+        /// Raised when the interstitial ad is ready to be shown
+        /// </summary>
+        public static Action<WSAInterstitialAdType> AdReady
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Raised when the user cancels the interstitial ad before it is considered complete
+        /// </summary>
+        public static Action<WSAInterstitialAdType> Cancelled
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Raised when the interstitial ad has been closed and the experience is considered complete
+        /// </summary>
+        public static Action<WSAInterstitialAdType> Completed
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Raised when the interstitial ad encounters an operational error
+        /// </summary>
+        public static Action<WSAInterstitialAdType> ErrorOccurred
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        public static Action<WSAInterstitialAdType, string, string> _Request;
+
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        public static Action<WSAInterstitialAdType> _Show;
+
+        private static string _adDuplexAppId;
+        private static string _adDuplexAdUnitId;
+
+        private static string _msAppId;
+        private static string _msAdUnitId;
+
+        private static string _vungleAppId;
+
+        /// <summary>
+        /// Initialise the interstitial ad for the specified provider
+        /// </summary>
+		/// <param name="adType">The type of ad to request</param>
+        /// <param name="appId">Your apps id</param>
+        /// <param name="adUnitId">Your apps ad unit id (null or empty for Vungle)</param>
+        public static void Initialise(WSAInterstitialAdType adType, string appId, string adUnitId)
+        {
+            switch (adType)
+            {
+                case WSAInterstitialAdType.AdDuplex:
+                    _adDuplexAppId = appId;
+                    _adDuplexAdUnitId = adUnitId;
+                    break;
+                case WSAInterstitialAdType.Microsoft:
+                    _msAppId = appId;
+                    _msAdUnitId = adUnitId;
+                    break;
+                case WSAInterstitialAdType.Vungle:
+                    _vungleAppId = appId;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Requests an interstitial ad from the server.
+        /// The AdReady event will fire when the request completes successfully.
+        /// Only needs to be called once for Vungle ads (ads will be automatically fetched from then on)
+        /// </summary>
+        /// <param name="adType">The type of ad to request</param>
+        public static void RequestAd(WSAInterstitialAdType adType)
+        {
+            if (_Request != null)
+            {
+                switch (adType)
+                {
+                    case WSAInterstitialAdType.AdDuplex:
+                        _Request(adType, _adDuplexAppId, _adDuplexAdUnitId);
+                        break;
+                    case WSAInterstitialAdType.Microsoft:
+                        _Request(adType, _msAppId, _msAdUnitId);
+                        break;
+                    case WSAInterstitialAdType.Vungle:
+                        _Request(adType, _vungleAppId, string.Empty);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shows the interstitial ad if it is ready.
+        /// It is recommended that you have at least a 60 second gap between ads
+        /// </summary>
+        /// <param name="adType">The type of ad to show</param>
+        public static void ShowAd(WSAInterstitialAdType adType)
+        {
+            if (_Show != null)
+            {
+                _Show(adType);
+            }
+        }
+
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="adType"></param>
+        public static void RaiseActionOnAppThread(Action<WSAInterstitialAdType> action, WSAInterstitialAdType adType)
+        {
+#if NETFX_CORE
+            if (action != null)
+            {
+                UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+                {
+                    action(adType);
+                }, false);
+            }
+#endif
         }
     }
 }
