07bd3cffe2d84a2074fca38c927a7d88f75f3c7f
Michael Clayton <mike-16@hotmail.co.uk>
2017-01-14 12:43:00 +0000

    Add dispatcher

---
+++ Source/Assets/WSANative/Dispatcher/WSANativeDispatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CI.WSANative.Dispatcher.Core;
+using UnityEngine;
+
+namespace CI.WSANative.Dispatcher
+{
+    public static class WSANativeDispatcher
+    {
+        private static WSADispatcher _dispatcher;
+
+        static WSANativeDispatcher()
+        {
+            if(_dispatcher == null)
+            {
+                _dispatcher = new GameObject("WSANativeDispatcher").AddComponent<WSADispatcher>();
+            }
+        }
+
+        public static void Invoke(Action action)
+        {
+            _dispatcher.Enqueue(action);
+        }
+    }
+}
