c4c3c87949007f7d29e7912b92dc891cd276ce62
Michael Clayton <michael.clayton@sky.uk>
2017-02-09 16:10:27 +0000

    Add personalisation

---
+++ Source/Assets/WSANative/Personalisation/WSANativePersonalisation.cs
@@ -0,0 +1,36 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//  
+// @module WSA Native for Unity3D 
+// @author Michael Clayton
+// @support clayton.inds+support@gmail.com 
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using UnityEngine;
+
+namespace CI.WSANative.Personalisation
+{
+    public static class WSANativePersonalisation
+    {
+        /// <summary>
+        /// For internal use only
+        /// </summary>
+        public static Func<Color> _GetAccentColour;
+
+        /// <summary>
+        /// Attempts to return the system accent colour
+        /// </summary>
+        /// <returns>The accent colour or white if it fails</returns>
+        public static Color GetAccentColour()
+        {
+#if UNITY_WSA_10_0
+            if (_GetAccentColour != null)
+            {
+                return _GetAccentColour();
+            }
+#endif
+            return Color.white;
+        }
+    }
+}
