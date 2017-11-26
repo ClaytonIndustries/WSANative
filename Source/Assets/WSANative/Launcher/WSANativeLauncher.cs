8d071080db8e6ffe5cda76e018a3c0eb0d090546
Michael Clayton <mike-16@hotmail.co.uk>
2017-11-26 13:29:44 +0000

    Update code comments

--- Source/Assets/WSANative/Launcher/WSANativeLauncher.cs
+++ Source/Assets/WSANative/Launcher/WSANativeLauncher.cs
@@ -15,7 +15,7 @@ namespace CI.WSANative.Launchers
     public static class WSANativeLauncher
     {
         /// <summary>
-        /// Starts the default app associated with the URI scheme name for the specified URI (for example specifying a url will launch the default browser)
+        /// Starts the default app associated with the uri scheme name for the specified uri (for example specifying a url will launch the default browser)
         /// </summary>
         /// <param name="uri">The uri</param>
         public static void LaunchUri(string uri)
