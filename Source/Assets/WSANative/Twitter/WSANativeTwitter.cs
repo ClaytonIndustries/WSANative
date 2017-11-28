6980e6dcef3962c366ac337535c9804295cfa555
Michael Clayton <mike-16@hotmail.co.uk>
2017-11-28 18:06:29 +0000

    Add code comments to twitter api

--- Source/Assets/WSANative/Twitter/WSANativeTwitter.cs
+++ Source/Assets/WSANative/Twitter/WSANativeTwitter.cs
@@ -20,6 +20,9 @@ namespace CI.WSANative.Twitter
         private static readonly WSATwitterApi _twitterApi = new WSATwitterApi();
 #endif
 
+        /// <summary>
+        /// Is the user currently logged in
+        /// </summary>
         public static bool IsLoggedIn
         {
 #if NETFX_CORE
@@ -29,6 +32,12 @@ namespace CI.WSANative.Twitter
 #endif
         }
 
+        /// <summary>
+        /// Initialise the twitter api - this must be called first - see the website for additional information
+        /// </summary>
+        /// <param name="consumerKey">Your apps twitter consumer key</param>
+        /// <param name="consumerSecret">Your apps twitter consumer secret</param>
+        /// /// <param name="oauthCallback">A callback url for oauth (should match the value entered on twitter)</param>
         public static void Initialise(string consumerKey, string consumerSecret, string oauthCallback)
         {
 #if NETFX_CORE
@@ -36,6 +45,11 @@ namespace CI.WSANative.Twitter
 #endif
         }
 
+        /// <summary>
+        /// Shows the login dialog to the user.
+        /// If login is successful an access token will be generated and automatically stored.
+        /// </summary>
+        /// <param name="response">Did the login request succeed</param>
         public static void Login(Action<WSATwitterLoginResult> response)
         {
 #if NETFX_CORE
@@ -54,6 +68,9 @@ namespace CI.WSANative.Twitter
 #endif
         }
 
+        /// <summary>
+        /// Log the user out of your application (i.e delete their access token)
+        /// </summary>
         public static void Logout()
         {
 #if NETFX_CORE
@@ -61,6 +78,11 @@ namespace CI.WSANative.Twitter
 #endif
         }
 
+        /// <summary>
+        /// Requests details about the logged in user - json response is returned (parse the fields you need)
+        /// </summary>
+        /// <param name="includeEmail">Should the response include the users email ("Request email addresses from users" must be set on twitter first)</param>
+        /// <param name="response">Response containing user details if successful</param>
         public static void GetUserDetails(bool includeEmail, Action<WSATwitterResponse> response)
         {
 #if NETFX_CORE
