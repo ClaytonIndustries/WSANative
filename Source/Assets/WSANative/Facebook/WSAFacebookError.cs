7d6d0b2c231036aaa3045c5974c07b586b61dba1
Michael Clayton <mike-16@hotmail.co.uk>
2017-11-28 22:51:44 +0000

    Update code comments on facebook

--- Source/Assets/WSANative/Facebook/WSAFacebookError.cs
+++ Source/Assets/WSANative/Facebook/WSAFacebookError.cs
@@ -10,9 +10,24 @@ namespace CI.WSANative.Facebook
 {
     public class WSAFacebookError
     {
+        /// <summary>
+        /// Message describing the error
+        /// </summary>
         public string Message { get; set; }
+
+        /// <summary>
+        /// The type of error
+        /// </summary>
         public string Type { get; set; }
+
+        /// <summary>
+        /// The error code
+        /// </summary>
         public string Code { get; set; }
+
+        /// <summary>
+        /// Is the users access token expired - if they so will need to login again
+        /// </summary>
         public bool AccessTokenExpired { get; set; }
     }
 }
